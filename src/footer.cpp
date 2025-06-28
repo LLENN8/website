c6589363af7d938ae079636bed5aa3ca764bd301
LLENN8 <46107821+LLENN8@users.noreply.github.com>
2025-06-28 15:07:57 +0700

    fix: "here" on footer going to source code

--- src/footer.cpp
+++ src/footer.cpp
@@ -6,7 +6,7 @@ Footer::Footer() {
     setStyleClass("footer");
 
     auto footer = addNew<Wt::WText>();
-    footer->setText("<hr/><p>Made with <span class=\"heart\">❤️</span> by Misa</p><p>Source code available <a href=\"#\">here</a></p>");
+    footer->setText("<hr/><p>Made with <span class=\"heart\">❤️</span> by Misa</p><p>Source code available <a href=\"https://github.com/LLENN8/website\">here</a></p>");
     footer->setTextFormat(Wt::TextFormat::XHTML);
 
 }
